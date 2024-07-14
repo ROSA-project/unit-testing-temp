00b6a36cc5be7544436df684aa36ad9bedabc99e
Saeed <saeedafra@Saeeds-MacBook-Air.local>
2024-07-15 00:45:03 +0200

    first draft to run googletest.

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "geometry_functions.h"
+
+int main()
+{
+    std::cout << sum_double(1,2.3) << std::endl;
+    return 0;
+}
---
+++ src/utils/geometry_functions.cpp
@@ -0,0 +1,4 @@
+double sum_double(double x, double y)
+{
+    return x + y;
+}
